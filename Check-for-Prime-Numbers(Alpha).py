8806e193f1eb05dbf7230e43d628325e274a4bb6
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2022-03-09 01:31:11 +0800

    Create Check-for-Prime-Numbers(Alpha).py

---
+++ Check-for-Prime-Numbers(Alpha).py
@@ -0,0 +1,48 @@
+print("欢迎使用《检验质数》！\n请输入一个大于等于2的整数”）开发者：Jacob_Hu\n本程序Gitee库地址:https://gitee.com/jacobhu0723/Check-for-Prime-Numbers\n如有Bug请在Gitee下Issues或评论区中进行反馈！\n")
+print("请输入一个大于等于2的整数！（输入其他字符即可结束运行）\n")
+
+while 1:
+        c=1
+        k=1
+        
+        try:
+             n=int(input())
+        except:
+            break
+        else:
+            
+            if n<1:
+                print("输入不合法！")
+                continue
+            if n==1:
+                print("1既不是质数也不是合数！")
+                continue
+            if n==2:
+                print("2是质数!")
+                continue
+            if n==3:
+                print("3是质数!")
+                continue
+            if n%2==0:
+                print(n,"是合数，因为它可以被2整除")
+                continue
+            if n%3==0:
+                print(n,"是合数，因为它可以被3整除")
+                continue
+            while (n/(6*k-1))>=(6*k-1) or (n/(6*k+1))>=(6*k+1):
+                if n%(6*k-1)==0:
+                    print(n,"是合数，因为它可以被",6*k-1,"整除")
+                    c=2
+                    break
+                if n%(6*k+1)==0:
+                    print(n,"是合数，因为它可以被",6*k+1,"整除")
+                    c=2
+                    break
+                k+=1
+            if c==1:
+                 print(n,"是质数！")
+                 continue
+      
+
+    
+print("\n感谢您的使用！")
